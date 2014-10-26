c7da04a2d0e0d5052e8bd2c5b3d67ec9ccfef336
Tom Pullen <tpullen21@gmail.com>
2014-10-26 18:02:55 +0000

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge
+//
+//  Created by Tom Pullen on 26/10/2014.
+//  Copyright (c) 2014 Tom Pullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSUserDefaults, Segues and Protocols Challenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSUserDefaults, Segues and Protocols Challenge
+//
+//  Created by Tom Pullen on 26/10/2014.
+//  Copyright (c) 2014 Tom Pullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
